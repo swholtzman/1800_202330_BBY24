0e4207dab4e043e45d4494358ab87e966ae632e2
Sam Holtzman <sam.wolf.holtzman@gmail.com>
2023-10-30 22:26:15 -0700

    ain

---
+++ scripts/main.js
@@ -0,0 +1,40 @@
+document.getElementById("cards").onmousemove = e => {
+    for(const card of document.getElementsByClassName("card")) {
+        const rect = card.getBoundingClientRect(),
+            x = e.clientX - rect.left,
+            y = e.clientY - rect.top;
+
+            
+        card.style.setProperty("--mouse-x", `${x}px`);
+        card.style.setProperty("--mouse-y", `${y}px`);
+
+        card.onclick = () => {
+            card.classList.toggle("active");
+        };
+    }
+}
+
+function getNameFromAuth() {
+    firebase.auth().onAuthStateChanged(user => {
+        // Check if a user is signed in:
+        if (user) {
+            // Do something for the currently logged-in user here: 
+            console.log(user.uid); //print the uid in the browser console
+            console.log(user.displayName);  //print the user name in the browser console
+            userName = user.displayName;
+
+            //method #1:  insert with JS
+            document.getElementById("name-goes-here").innerText = userName;    
+
+            //method #2:  insert using jquery
+            //$("#name-goes-here").text(userName); //using jquery
+
+            //method #3:  insert using querySelector
+            //document.querySelector("#name-goes-here").innerText = userName
+
+        } else {
+            // No user is signed in.
+        }
+    });
+}
+getNameFromAuth(); //run the function
