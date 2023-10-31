f9b8d1da1d86b56807770a1b31f22f5263c6a1a4
Sam Holtzman <144474473+swholtzman@users.noreply.github.com>
2023-10-30 18:34:05 -0700

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -22,3 +22,20 @@ function getNameFromAuth() {
     });
 }
 getNameFromAuth(); //run the function
+
+
+document.getElementById("cards").onmousemove = e => {
+    for(const card of document.getElementsByClassName("card")) {
+        const rect = card.getBoundingClientRect(),
+            x = e.clientX - rect.left,
+            y = e.clientY - rect.top;
+
+            
+        card.style.setProperty("--mouse-x", `${x}px`);
+        card.style.setProperty("--mouse-y", `${y}px`);
+
+        card.onclick = () => {
+            card.classList.toggle("active");
+        };
+    }
+}
