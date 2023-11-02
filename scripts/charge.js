cbfdafdabcb7cb47119f82df764986e2382740f4
Tyson C <hchan205@my.bcit.ca>
2023-11-02 10:18:58 -0700

    leave charging page by clicking body element

--- scripts/charge.js
+++ scripts/charge.js
@@ -3,3 +3,12 @@ function leave(){
     document.getElementById('map').classList.add('leave');
     setTimeout( function () { location.href = "main.html"; }, 1000);
 }
+
+var cards = document.getElementById('cards');
+
+window.addEventListener("click", function(event) {
+    if (event.target.nodeName == "BODY"){
+        leave();
+    }
+});
+
