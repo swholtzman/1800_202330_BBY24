659b8b1bb8256895003b18191795aca9462bd0b1
Rodrigo Rocha <rodrigo@vancouver4us.com>
2023-11-07 00:10:12 -0800

    Overhauled settings page

---
+++ scripts/settings.js
@@ -0,0 +1,11 @@
+function toggle(switchElement) {
+  switchElement.classList.toggle("active");
+}
+
+function addHeader(title) {}
+
+function addSection(title) {}
+
+function addSwitch(name, defaultState) {}
+
+function addLink(name, destination) {} 
