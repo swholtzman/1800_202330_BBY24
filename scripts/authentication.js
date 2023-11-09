f4e6aded58d917f8fe1c18046d4a68cba10cfe2d
Rodrigo Rocha <rodrigo@vancouver4us.com>
2023-11-09 13:36:34 -0800

    Added user.uid to localStorage

--- scripts/authentication.js
+++ scripts/authentication.js
@@ -15,11 +15,12 @@ var uiConfig = {
         // Before this works, you must enable "Firestore" from the firebase console.
         // The Firestore rules must allow the user to write. 
         //------------------------------------------------------------------------------------------
-        var user = authResult.user;                            // get the user object from the Firebase authentication database
+        var user = authResult.user;                          // get the user object from the Firebase authentication database
+        localStorage.setItem("currentUid", user.uid);
         if (authResult.additionalUserInfo.isNewUser) {         //if new user
             db.collection("users").doc(user.uid).set({         //write to firestore. We are using the UID for the ID in users collection
                    name: user.displayName,                    //"users" collection
-                   email: user.email,                         //with authenticated user's ID (user.uid)
+                   email: user.email                         //with authenticated user's ID (user.uid)
                    
             }).then(function () {
                    console.log("New user added to firestore");
