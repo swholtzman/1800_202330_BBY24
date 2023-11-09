8ecd7e23669bab7369f9665b98efde1603a2d127
Tyson C <hchan205@my.bcit.ca>
2023-11-09 09:03:54 -0800

    charging page stuff

--- scripts/main.js
+++ scripts/main.js
@@ -20,6 +20,7 @@ document.getElementById("cards").onmousemove = e => {
 
 let centerButton = document.querySelector('.centerButton');
 centerButton.onclick = function(){
+    console.log("a");
     centerButton.classList.toggle('active');
 }
 
