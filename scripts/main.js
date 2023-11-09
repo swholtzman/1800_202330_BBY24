25e75002be387fb7030f6d5b9025c68c73d80da6
Tyson C <hchan205@my.bcit.ca>
2023-11-09 09:36:24 -0800

    transition between main and charge/ map page

--- scripts/main.js
+++ scripts/main.js
@@ -27,10 +27,11 @@ hamburgerButton.onclick = function(){
     hamburgerButton.classList.toggle('active');
 }
 
-document.querySelector("#charge").addEventListener("click", function(event) {
+document.querySelector("#toCharge").addEventListener("click", function(event) {
     location.href = "charge.html";
 });
 
-document.querySelector("#map").addEventListener("click", function(event) {
+document.querySelector("#toMap").addEventListener("click", function(event) {
     location.href = "map.html";
 });
+
