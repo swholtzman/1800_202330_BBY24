0f86b085c22a643310722a13a23e465087f91141
Sam Holtzman <sam.wolf.holtzman@gmail.com>
2023-11-11 15:25:01 -0800

    Stylistic Alterations for Inter-Page Tranitions

--- scripts/charge.js
+++ scripts/charge.js
@@ -42,3 +42,34 @@ function displayEstimatedTime() {
 }
 displayEstimatedTime("pxyOYQRUt1XAkIME9rH8oDUUvbI3")
 
+
+let centerButton = document.querySelector('.centerButton');
+centerButton.onclick = function(){
+    console.log("a");
+    centerButton.classList.toggle('active');
+}
+
+let hamburgerButton = document.querySelector('.hamburgerButton');
+hamburgerButton.onclick = function(){
+    hamburgerButton.classList.toggle('active');
+}
+
+let hamburgerButton = document.querySelector('.hamburgerButton');
+hamburgerButton.onclick = function(){
+    hamburgerButton.classList.toggle('active');
+}
+
+const primaryNav = document.querySelector(".primary-navigation");
+
+hamburgerButton.addEventListener('click', (event) => {
+    const visibility = primaryNav.getAttribute("data-visible");
+
+    if (visibility === "false") {
+        primaryNav.setAttribute("data-visible", "true")
+        hamburgerButton.setAttribute("data-visible", "true");
+    } else if (visibility === "true") {
+        primaryNav.setAttribute("data-visible", "false");
+        hamburgerButton.setAttribute("data-visible", "false");
+    }
+});
+
