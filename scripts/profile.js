d57a14fd0ae4907d1d2202d6eea9807062ce2fa6
Sam Holtzman <sam.wolf.holtzman@gmail.com>
2023-11-20 22:20:06 -0800

    fixed issues with readability when editing profile

--- scripts/profile.js
+++ scripts/profile.js
@@ -1,3 +1,20 @@
+// Store initial values
+const initialStyles = {
+    nameInput: {
+        backgroundColor: getComputedStyle(document.getElementById("nameInput")).getPropertyValue('background-color'),
+        color: getComputedStyle(document.getElementById("nameInput")).getPropertyValue('color')
+    },
+    carInput: {
+        backgroundColor: getComputedStyle(document.getElementById("carInput")).getPropertyValue('background-color'),
+        color: getComputedStyle(document.getElementById("carInput")).getPropertyValue('color')
+    },
+    cityInput: {
+        backgroundColor: getComputedStyle(document.getElementById("cityInput")).getPropertyValue('background-color'),
+        color: getComputedStyle(document.getElementById("cityInput")).getPropertyValue('color')
+    }
+};
+
+
 var currentUser;               //points to the document of the user who is logged in
 function populateUserInfo() {
     firebase.auth().onAuthStateChanged(user => {
@@ -77,3 +94,36 @@ function saveUserInfo() {
 function updateCarModel(model) {
   db.collection("users").doc(user.uid).collection("charge_info").doc("car").update({car: model});
 }
+
+function editUserInfo() {
+    // Enable the form fields for editing
+    document.getElementById("personalInfoFields").disabled = false;
+
+    // Change the background color and text color of input fields
+    document.getElementById("nameInput").style.backgroundColor = "var(--clr-off-white)";
+    document.getElementById("nameInput").style.color = "blue";
+
+    document.getElementById("carInput").style.backgroundColor = "var(--clr-off-white)";
+    document.getElementById("carInput").style.color = "blue";
+
+    document.getElementById("cityInput").style.backgroundColor = "var(--clr-off-white)";
+    document.getElementById("cityInput").style.color = "blue";
+}
+
+function saveUserInfo() {
+    // Disable the form fields after saving
+    document.getElementById("personalInfoFields").disabled = true;
+
+    // Restore the initial background color and text color of input fields
+    document.getElementById("nameInput").style.backgroundColor = initialStyles.nameInput.backgroundColor;
+    document.getElementById("nameInput").style.color = initialStyles.nameInput.color;
+
+    document.getElementById("carInput").style.backgroundColor = initialStyles.carInput.backgroundColor;
+    document.getElementById("carInput").style.color = initialStyles.carInput.color;
+
+    document.getElementById("cityInput").style.backgroundColor = initialStyles.cityInput.backgroundColor;
+    document.getElementById("cityInput").style.color = initialStyles.cityInput.color;
+}
+
+
+
