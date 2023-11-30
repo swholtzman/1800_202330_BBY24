b8e094c04e846289aa94aa85cdb92b05db694e00
BeckTheShrek <beckharper13@gmail.com>
2023-11-30 12:08:05 -0800

    Implemented the live serve hosting to create a clickable link. Beck

--- scripts/charge.js
+++ scripts/charge.js
@@ -1,7 +1,7 @@
 function leave() {
   document.getElementById('charge').classList.add('leave');
   document.getElementById('map').classList.add('leave');
-  setTimeout(function () { location.href = "main.html"; }, 1000);
+  setTimeout(function () { location.href = "index.html"; }, 1000);
 }
 
 var cards = document.getElementById('cards');
